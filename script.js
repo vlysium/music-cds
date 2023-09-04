c71ef9138e2d0712f94c49d1ffe4697c31108369
Victor Ly <ly.victor2000@gmail.com>
2023-09-04 12:49:30 +0200

    added data display functionality

--- script.js
+++ script.js
@@ -5,7 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const allMusicCDs = []; // list of all music CDs to be displayed
 
-  const MusicCD = { author: null, title: null, year: null, }; // music CD prototype
+  const MusicCD = { author: null, title: null, year: null, }; // music CD object prototype
+
+  const musicCDList = document.querySelector("#cd-list"); // parent element for the music CDs
+
+  const musicCDTemplate = document.querySelector("#cd-template"); // template fragment for the music CD
 
   const musicCDForm = document.querySelector("#cd-form");
   musicCDForm.addEventListener("submit", submitForm);
@@ -14,6 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const musicCDTitle = musicCDForm.querySelector("#form-title");
   const musicCDYear = musicCDForm.querySelector("#form-year");
   
+  // on form submit
   function submitForm(e) {
     e.preventDefault();
     
@@ -22,6 +27,8 @@ document.addEventListener("DOMContentLoaded", () => {
     allMusicCDs.push(formData);
 
     resetForm();
+
+    displayData(allMusicCDs);
   }
 
   // retrieve data from the form
@@ -47,5 +54,27 @@ document.addEventListener("DOMContentLoaded", () => {
     musicCDTitle.value = "";
     musicCDYear.value = "";
   }
+
+  // display the music CD list
+  function displayData(musicCDs) {
+    // clear the list
+    musicCDList.innerHTML = "";
+
+    // loop through allMusicCDs and "render" the data
+    musicCDs.forEach((musicCD) => {
+      const musicCDClone = document.importNode(musicCDTemplate.content, true);
+
+      const musicCDCloneAuthor = musicCDClone.querySelector("[data-cd-author]");
+      musicCDCloneAuthor.textContent = musicCD.author;
+
+      const musicCDCloneTitle = musicCDClone.querySelector("[data-cd-title]");
+      musicCDCloneTitle.textContent = musicCD.title;
+
+      const musicCDCloneYear = musicCDClone.querySelector("[data-cd-year]");
+      musicCDCloneYear.textContent = musicCD.year;
+
+      musicCDList.appendChild(musicCDClone);
+    })
+  };
 
 });
